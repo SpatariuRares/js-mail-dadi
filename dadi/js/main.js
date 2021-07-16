12cba0a78a71e8d343b4f8739dcc2432e75010fc
spata00 <sararares12@gmail.com>
2021-07-16 13:16:10 +0200

    dadi

---
+++ dadi/js/main.js
@@ -0,0 +1,12 @@
+var numero_giocatore = Math.floor(Math.random() * 6) +1;
+var numero_pc = Math.floor(Math.random() * 6) +1;
+
+if (numero_giocatore>numero_pc){
+    document.getElementById("message").innerHTML="hai fatto " + numero_giocatore +" il pc ha fatto " + numero_pc+" hai vinto"
+}else if (numero_giocatore<numero_pc){
+    document.getElementById("message").innerHTML="hai fatto " + numero_giocatore +" il pc ha fatto " + numero_pc+" hai perso"
+}else{
+    document.getElementById("message").innerHTML="hai fatto " + numero_giocatore +" il pc ha fatto " + numero_pc+" hai parregiato"
+}
+
+
