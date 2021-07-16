39c49938a9da1ca3deb8c76b33f3f24bb7902bb1
spata00 <sararares12@gmail.com>
2021-07-16 18:22:54 +0200

    ads

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,6 +1,5 @@
 var emails = ["spatariurares@gmail.com","sararares12@gmail.com"];
 var user_email = prompt("insert a emails");
-var flag =0;
 for (var i = 0; i < emails.length && user_email!=emails[i];i++) {}
 if(i!=emails.length){
     document.getElementById("message").innerHTML="entra";
